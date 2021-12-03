da2cf751f05732f8e393dc3fab9065f6368caa99
Nisan Dalva <nisandalva@gmail.com>
2021-12-03 13:33:53 +0200

    initial commit

---
+++ src/main/java/com/covid19Daily/Covid19DailyApplication.java
@@ -0,0 +1,13 @@
+package com.covid19Daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19DailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19DailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid19Daily/Covid19DailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid19Daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19DailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
