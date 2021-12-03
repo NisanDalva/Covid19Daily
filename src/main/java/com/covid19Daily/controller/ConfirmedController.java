4bcdc0be5b18c84870f47ba63ae55099631b993a
Nisan Dalva <nisandalva@gmail.com>
2021-12-04 00:11:43 +0200

    build a controller for confirmed cases

---
+++ src/main/java/com/covid19Daily/controller/ConfirmedController.java
@@ -0,0 +1,39 @@
+package com.covid19Daily.controller;
+
+import java.util.ArrayList;
+
+import com.covid19Daily.logic.ConfirmedLogicImplementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/confirmed")
+public class ConfirmedController {
+
+    private ConfirmedLogicImplementation confirmedLogicImplementation;
+
+    @Autowired
+    public void setConfirmedLogicImplementation(ConfirmedLogicImplementation confirmedLogicImplementation) {
+        this.confirmedLogicImplementation = confirmedLogicImplementation;
+    }
+    
+    
+
+    @RequestMapping(
+			path = "/daily",
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+    public Integer getConfirmedCases(
+        @RequestParam(name = "country", required = true) String country,
+        @RequestParam(name = "date", required = true) String date
+    ) {
+        // STUB implementation
+        return 0;
+    }
+    
+}
