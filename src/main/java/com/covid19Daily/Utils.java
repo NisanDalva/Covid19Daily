328d680f578299954a7e7b5ca11e2e08af9397f4
Nisan Dalva <nisandalva@gmail.com>
2021-12-04 17:10:47 +0200

    added more utils functions

--- src/main/java/com/covid19Daily/Utils.java
+++ src/main/java/com/covid19Daily/Utils.java
@@ -2,6 +2,7 @@ package com.covid19Daily;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -22,21 +23,13 @@ public class Utils {
 
 
     /**
-     * Parse a string presents a date and convert it to {@code Date} object
+     * Check and parse a string that presents a date and convert it to {@code Date} object
      * @param date - A simple date representing in string format
-     * @return {@code Date} - If a given date is a correct date and in a correct
-     * format, otherwise - {@code null}
+     * @return {@code Date} - The same date as an object
+     * @throws ParseException - if a date is incorrect or in incorrect format
      */
-    public Date parseDate(String date) {
-        Date dateObj;
-
-        try {
-            dateObj = formatter.parse(date);
-        } catch (ParseException e) {
-            dateObj = null;
-        }
-
-        return dateObj;
+    public Date parseDate(String date) throws ParseException {
+        return formatter.parse(date);
     }
 
     /**
@@ -51,4 +44,39 @@ public class Utils {
         
         return calendar.getTime();
     }
+    
+    /**
+     * 
+     * @param date - A simple date
+     * @return {@code Date} - The next day 
+     */
+    private Date getNextDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, 1);
+        
+        return calendar.getTime();
+    }
+
+    /**
+     * Get all dates between two different dates
+     * @param from - The Start date
+     * @param to - The end date INCLUDING
+     * @return {@code ArrayList<String>} contains all the dates in format that fits to {@code formater} object
+     */
+    public ArrayList<String> getDatesRange(Date from, Date to) {
+        SimpleDateFormat apiFormatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        ArrayList<String> rv = new ArrayList<>();
+        to = getNextDay(to);
+        
+        while (from.before(to)) {
+            rv.add(apiFormatter.format(from));
+            from = getNextDay(from);
+        }
+
+        return rv;
+    }
+
+
 }
